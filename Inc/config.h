96ea1f9e7649ded97395f14bd6e24dd08dccbc60
tetrap1 <133867726+tetrap1@users.noreply.github.com>
2024-05-16 14:39:19 +0200

    VLT mode, hovrercar i 100 obroty

--- Inc/config.h
+++ Inc/config.h
@@ -526,7 +526,7 @@
   // #define ELECTRIC_BRAKE_THRES  120         // (0, 500) Threshold below at which the electric brake starts engaging.
 
   //#define MULTI_MODE_DRIVE                  // This option enables the selection of 3 driving modes at start-up using combinations of Brake and Throttle pedals (see below)
- // #ifdef MULTI_MODE_DRIVE
+ #ifdef MULTI_MODE_DRIVE
       // BEGINNER MODE:     Power ON + Brake [released] + Throttle [released or pressed]
       #define MULTI_MODE_DRIVE_M1_MAX   175
       #define MULTI_MODE_DRIVE_M1_RATE  150
